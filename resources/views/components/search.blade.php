423d9bd31975094c8973a639ab66523760b3eed5
Walter <walter@MacBook-Pro-de-Walter.local>
2023-03-18 23:26:16 -0500

    feat: search styles

---
+++ resources/views/components/search.blade.php
@@ -0,0 +1,4 @@
+@props(['size' => 'text-2xl', 'color' => 'text-white'])
+
+
+<i class='fa fa-search {{ $size }} {{ $color }}'></i>
--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -1,3 +1,6 @@
-<div class="flex-1">
-  <x-jet-input type="text" class="flex w-full" placeholder="¿Estas buscando algun producto?"/>
+<div class="flex-1 flex justify-center items-center">
+  <x-jet-input type="text" class="flex w-full border-0 rounded-r-none" placeholder="¿Estas buscando algun producto?"/>
+    <button class="bg-orange-600 w-100 py-1 px-3  rounded-r-md ">
+       <x-search/>
+    </button>
 </div>
